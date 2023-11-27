933572f566804af4f045e98e7ae872c0661c811e
Kovács Tamás <144217056+kovacs-tamas2004@users.noreply.github.com>
2023-11-27 11:27:11 +0100

    Add files via upload

---
+++ fetch.js
@@ -0,0 +1,7 @@
+fetch("https://api.quotable.io/quotes/random")
+    .then(response => response.json())
+    .then(data => {
+    console.log(data[0].author);
+    console.log(data[0].content);
+})
+.catch(error => console.error("Hiba lépett fel" + error))
